dd3d2de2b1160c05d3f10189019705a8cbfd2183
eaglesareedible <zadler2029@kehillahstudent.org>
2025-12-04 09:22:15 -0800

    Commiting

--- main.py
+++ main.py
@@ -1,8 +1,13 @@
-from simulator import robot, FORWARD, BACKWARD, STOP
+from simulator import robot,FORWARD, BACKWARD, STOP
 
 # TODO: Write your code here!
 # Use robot.motors() to move
 # Use robot.left_sonar() and robot.right_sonar() to sense obstacles
+robot.motors(FORWARD, FORWARD, 10)
+robot.left_sonar
+robot.right_sonar
 
 # When you're done, close the simulator
 robot.exit()
+
+
