f637b4e6e28462b430a9e303d93a8b87cb8c42a7
eaglesareedible <zadler2029@kehillahstudent.org>
2025-12-04 09:22:23 -0800

    Oops

--- main.py
+++ main.py
@@ -3,11 +3,17 @@ from simulator import robot,FORWARD, BACKWARD, STOP
 # TODO: Write your code here!
 # Use robot.motors() to move
 # Use robot.left_sonar() and robot.right_sonar() to sense obstacles
-robot.motors(FORWARD, FORWARD, 10)
-robot.left_sonar
-robot.right_sonar
+yes = True
+robot.motors(FORWARD, STOP, 1.7)
+for i in range(100):
+	robot.motors(FORWARD, FORWARD, 1)
+	if robot.left_sonar()<15:
+		robot.motors(FORWARD, STOP, 4.5)
+		robot.motors(FORWARD, FORWARD, 1)
+	elif robot.right_sonar()<15:
+            robot.motors(STOP, FORWARD, 4.5)
+            robot.motors(FORWARD, FORWARD, 1)
 
-# When you're done, close the simulator
 robot.exit()
 
 
