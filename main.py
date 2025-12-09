a1693b1917f5b2ab24c62b99c49f5713f46eb3af
eaglesareedible <zadler2029@kehillahstudent.org>
2025-12-09 10:23:14 -0800

    Uploading

--- main.py
+++ main.py
@@ -3,16 +3,44 @@ from simulator import robot,FORWARD, BACKWARD, STOP
 # TODO: Write your code here!
 # Use robot.motors() to move
 # Use robot.left_sonar() and robot.right_sonar() to sense obstacles
-yes = True
-robot.motors(FORWARD, STOP, 1.7)
-for i in range(100):
-	robot.motors(FORWARD, FORWARD, 1)
-	if robot.left_sonar()<15:
-		robot.motors(FORWARD, STOP, 4.5)
+# Robot travels at speed of 6cm/s 
+def square():
+	for i in range(4):
+		forwardwithsonar(3)
+		robot.motors(FORWARD, STOP, 3.05)
+def spinright(time):
+	robot.motors(FORWARD, BACKWARD, time)
+def spinleft(time):
+	robot.motors(BACKWARD, FORWARD, time)
+def screenSaver():
+	while True:
 		robot.motors(FORWARD, FORWARD, 1)
-	elif robot.right_sonar()<15:
-            robot.motors(STOP, FORWARD, 4.5)
-            robot.motors(FORWARD, FORWARD, 1)
+		if robot.left_sonar()<15:
+			robot.motors(FORWARD, BACKWARD, 2)
+			robot.motors(FORWARD, FORWARD, 1)
+		elif robot.right_sonar()<15:
+				robot.motors(BACKWARD, FORWARD, 2)
+				robot.motors(FORWARD, FORWARD, 1)
+		elif robot.right_sonar()<5:
+			break
+		elif robot.left_sonar_sonar()<5:
+			break
+		userInput = input("Press enter in order to end screen saver")
+		if not userInput:
+			print("Ending screen saver")
+			break
+def forwardwithsonar(howlong):
+	for i in range(howlong):
+		robot.motors(FORWARD, FORWARD, 1)
+		if robot.left_sonar()<9:
+			robot.motors(BACKWARD, BACKWARD, 3)
+		elif robot.right_sonar()<9:
+				robot.motors(BACKWARD, BACKWARD, 3)
+		elif robot.right_sonar()<3:
+			break
+		elif robot.left_sonar()<3:
+			break
+screenSaver()
 
 robot.exit()
 
