87597273d4c52032e1ce86141e003f7a2f278baf
eaglesareedible <zadler2029@kehillahstudent.org>
2025-12-11 09:17:06 -0800

    Committing

--- main.py
+++ main.py
@@ -4,14 +4,27 @@ from simulator import robot,FORWARD, BACKWARD, STOP
 # Use robot.motors() to move
 # Use robot.left_sonar() and robot.right_sonar() to sense obstacles
 # Robot travels at speed of 6cm/s 
+def forwardwithsonar(howlong):
+	"This function does forward"
+	for i in range(howlong):
+		robot.motors(FORWARD, FORWARD, 1)
+		if robot.left_sonar()<9:
+			robot.motors(BACKWARD, BACKWARD, 3)
+		elif robot.right_sonar()<9:
+				robot.motors(BACKWARD, BACKWARD, 3)
+		elif robot.right_sonar()<3:
+			break
+		elif robot.left_sonar()<3:
+			break
+
 def square():
 	for i in range(4):
 		forwardwithsonar(3)
 		robot.motors(FORWARD, STOP, 3.05)
-def spinright(time):
-	robot.motors(FORWARD, BACKWARD, time)
-def spinleft(time):
-	robot.motors(BACKWARD, FORWARD, time)
+def spinright(spinrighttime):
+	robot.motors(FORWARD, BACKWARD, spinrighttime)
+def spinleft(spinlefttime):
+	robot.motors(BACKWARD, FORWARD, spinlefttime)
 def screenSaver():
 	while True:
 		robot.motors(FORWARD, FORWARD, 1)
@@ -29,19 +42,17 @@ def screenSaver():
 		if not userInput:
 			print("Ending screen saver")
 			break
-def forwardwithsonar(howlong):
-	for i in range(howlong):
-		robot.motors(FORWARD, FORWARD, 1)
-		if robot.left_sonar()<9:
-			robot.motors(BACKWARD, BACKWARD, 3)
-		elif robot.right_sonar()<9:
-				robot.motors(BACKWARD, BACKWARD, 3)
-		elif robot.right_sonar()<3:
-			break
-		elif robot.left_sonar()<3:
-			break
-screenSaver()
-
-robot.exit()
-
-
+userInput = input("Select one: \n square\nspinright\nspinleft\nscreensaver\nforward\nanything else to exit\n")
+if userInput=='square':
+	square()
+elif userInput=='spinright':
+	spintime = int(input("How many seconds should it spin for?"))
+	spinright(spintime)
+elif userInput=='spinleft':
+	spintime = int(input("How many seconds should it spin for?"))
+	spinleft(spintime)
+elif userInput=='screensaver':
+	screenSaver()
+else:
+	print("Breaking")
+	robot.exit()
